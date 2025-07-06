a26b8266ffac93efda0ea376b9842c5b0d9f7362
Aditijain114 <117623836+Aditijain114@users.noreply.github.com>
2025-07-06 22:16:36 +0530

    Add files via upload

---
+++ ResUNet.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import random
+import numpy as np
+from glob import glob
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models import resnet34, ResNet34_Weights
+
+import torch.cuda.amp as amp
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+import torch.cuda.amp as amp
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+# In[2]:
+
+
+# In[3]:
+
+
+#channel attention block
+class ChannelAttention(nn.Module):
+    def __init__(self, in_channels, reduction_ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels // reduction_ratio, 1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(in_channels // reduction_ratio, in_channels, 1, bias=False)
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc(self.avg_pool(x))
+        max_out = self.fc(self.max_pool(x))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+        
+#spatial attention block
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        self.conv = nn.Conv2d(2, 1, kernel_size, padding=kernel_size//2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x_out = torch.cat([avg_out, max_out], dim=1)
+        x_out = self.conv(x_out)
+        return self.sigmoid(x_out)
+
+#ASPP module
+class ASPP(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ASPP, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels, out_channels//4, kernel_size=1)
+        self.conv2 = nn.Conv2d(in_channels, out_channels//4, kernel_size=3, padding=3, dilation=3)
+        self.conv3 = nn.Conv2d(in_channels, out_channels//4, kernel_size=3, padding=6, dilation=6)
+        self.conv4 = nn.Conv2d(in_channels, out_channels//4, kernel_size=3, padding=12, dilation=12)
+
+
+
+        self.bn1 = nn.BatchNorm2d(out_channels//4)
+        self.bn2 = nn.BatchNorm2d(out_channels//4)
+        self.bn3 = nn.BatchNorm2d(out_channels//4)
+        self.bn4 = nn.BatchNorm2d(out_channels//4)
+
+
+        self.conv_out = nn.Conv2d(out_channels, out_channels, kernel_size=1)
+        self.bn_out = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x1 = self.relu(self.bn1(self.conv1(x)))
+        x2 = self.relu(self.bn2(self.conv2(x)))
+        x3 = self.relu(self.bn3(self.conv3(x)))
+        x4 = self.relu(self.bn4(self.conv4(x)))
+
+
+        x_out = torch.cat([x1, x2, x3, x4], dim=1)
+        x_out = self.relu(self.bn_out(self.conv_out(x_out)))
+        return x_out
+
+
+# In[4]:
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, skip_channels, out_channels):
+        super(DecoderBlock, self).__init__()
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels + skip_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+       
+        self.ca = ChannelAttention(skip_channels)
+        self.sa = SpatialAttention()
+
+    def forward(self, x, skip):
+        x = self.up(x)
+
+       
+        diffY = skip.size()[2] - x.size()[2]
+        diffX = skip.size()[3] - x.size()[3]
+        x = F.pad(x, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
+
+       
+        skip = skip * self.ca(skip)
+        skip = skip * self.sa(skip)
+
+       
+        x = torch.cat([x, skip], dim=1)
+        return self.conv(x)
+
+
+# In[5]:
+
+
+class ResUNet(nn.Module):
+    def __init__(self, in_channels, num_classes, pretrained=True):
+        super(ResUNet, self).__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        
+        # Load ResNet34 backbone
+        resnet = resnet34(weights=None)
+        state_dict = torch.load('./resnet34.pth')  
+        resnet.load_state_dict(state_dict)
+
+        # Encoder
+        self.firstconv = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.firstbn = resnet.bn1
+        self.firstrelu = resnet.relu
+        self.firstmaxpool = resnet.maxpool
+        self.encoder1 = resnet.layer1  # 64
+        self.encoder2 = resnet.layer2  # 128
+        self.encoder3 = resnet.layer3  # 256
+        self.encoder4 = resnet.layer4  # 512
+
+        # Center
+        self.center = ASPP(512, 512)
+
+        # Decoder
+        self.decoder4 = DecoderBlock(512, 256, 256)
+        self.decoder3 = DecoderBlock(256, 128, 128)
+        self.decoder2 = DecoderBlock(128, 64, 64)
+        self.decoder1 = DecoderBlock(64, 64, 32)
+
+        # Segmentation heads
+        self.dsv4 = nn.Conv2d(256, num_classes, kernel_size=1)
+        self.dsv3 = nn.Conv2d(128, num_classes, kernel_size=1)
+        self.dsv2 = nn.Conv2d(64, num_classes, kernel_size=1)
+        self.dsv1 = nn.Conv2d(32, num_classes, kernel_size=1)
+        self.final = nn.Conv2d(32, num_classes, kernel_size=1)
+        
+        # Classification head (using features from encoder4)
+        self.classifier_head = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Flatten(),
+            nn.Linear(512, 5),  # 5 classes
+            nn.Softmax(dim=1)
+        )
+
+        # Router (using features from encoder4)
+        self.router = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Flatten(),
+            nn.Linear(512, 128),
+            nn.ReLU(),
+            nn.Linear(128, 2),  # Two tasks
+            nn.Softmax(dim=1)
+        )
+      
+    def forward(self, x):
+        input_size = x.size()[2:]
+        
+        # Encoder
+        x = self.firstconv(x)
+        x = self.firstbn(x)
+        x = self.firstrelu(x)
+        x_skip1 = x  # 64ch, 1/2
+        x = self.firstmaxpool(x)
+
+        x = self.encoder1(x)
+        x_skip2 = x  # 64ch, 1/4
+
+        x = self.encoder2(x)
+        x_skip3 = x  # 128 ch, 1/8
+
+        x = self.encoder3(x)
+        x_skip4 = x  # 256 ch, 1/16
+
+        x4 = self.encoder4(x)  # 512ch, 1/32
+
+
+        # Segmentation path
+        #x = self.center(x4)
+
+        # Get routing weights
+        routing_weights = self.router(x4)
+        topk_val, topk_idx = torch.topk(routing_weights, k=1, dim=-1)  # [B, 1]
+        
+        x = self.decoder4(x4, x_skip4)
+        d4 = x  
+
+        x = self.decoder3(x, x_skip3)
+        d3 = x 
+
+        x = self.decoder2(x, x_skip2)
+        d2 = x  
+
+        x = self.decoder1(x, x_skip1)
+        d1 = x  
+
+        # Final segmentation outputs
+        final = self.final(x)
+        final = F.interpolate(final, size=input_size, mode='bilinear', align_corners=True)
+        
+        # Multi-scale outputs
+        p4 = self.dsv4(d4)
+        p4 = F.interpolate(p4, size=input_size, mode='bilinear', align_corners=True)
+        
+        p3 = self.dsv3(d3)
+        p3 = F.interpolate(p3, size=input_size, mode='bilinear', align_corners=True)
+        
+        p2 = self.dsv2(d2)
+        p2 = F.interpolate(p2, size=input_size, mode='bilinear', align_corners=True)
+        
+        p1 = self.dsv1(d1)
+        p1 = F.interpolate(p1, size=input_size, mode='bilinear', align_corners=True)
+
+        # Classification output
+        class_outputs = self.classifier_head(x4)
+
+        # Final output decision per sample
+        batch_size = x.shape[0]
+        classification_final = []
+        segmentation_final = []
+    
+        for i in range(batch_size):
+            if topk_idx[i, 0] == 0:  # classification expert selected
+                classification_final.append(class_outputs[i].unsqueeze(0))
+                segmentation_final.append(None)
+            else:  # segmentation expert selected
+                classification_final.append(None)
+                segmentation_final.append([
+                    p4[i].unsqueeze(0),
+                    p3[i].unsqueeze(0),
+                    p2[i].unsqueeze(0),
+                    p1[i].unsqueeze(0),
+                    final[i].unsqueeze(0)
+                ])
+    
+        if self.training:
+            return {
+                'segmentation': segmentation_final,
+                'classification': classification_final,
+                'routing_weights': routing_weights,
+                'active_expert': topk_idx,
+                'deep_supervision': [p1, p2, p3, p4, final],
+                'only_classification': class_outputs
+            }
+        else:
+            return {
+                'segmentation': segmentation_final,
+                'classification': classification_final,
+                'routing_weights': routing_weights,
+                'active_expert': topk_idx,
+                'only_classification': class_outputs
+            }
+
+
+# In[6]:
+
+
+net = ResUNet(in_channels=3, num_classes=5, pretrained=True)
+
+
+# In[7]:
+
+
+#from torchsummary import summary
+
+
+# In[8]:
+
+
+#net.to('cuda' if torch.cuda.is_available() else 'cpu') 
+#summary(net, input_size=(3, 224, 224))
+
+
+# In[ ]:
+
+
+
+
---
+++ dataloader.py
@@ -0,0 +1,102 @@
+import os
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision.transforms as transforms
+import torchvision.transforms.functional as F
+
+class IDRiD_Dataset(Dataset):
+    def __init__(self, image_root, csv_path, mask_root, trainsize, augmentations=False, mask_type='binary'):
+        self.trainsize = trainsize
+        self.augmentations = augmentations
+        self.mask_type = mask_type
+        
+    
+        self.csv = pd.read_csv(csv_path)
+        self.csv['Image name'] = self.csv['Image name'].str.replace(".jpg", "", case=False).apply(lambda x: "IDRiD_" + str(int(x.split('_')[-1])).zfill(2))
+        available_imgs = set([f.split(".")[0] for f in os.listdir(image_root)])
+        self.csv = self.csv[self.csv['Image name'].isin(available_imgs)].reset_index(drop=True)
+        self.image_paths = [os.path.join(image_root, f"{img_id}.jpg") for img_id in self.csv['Image name']]
+        self.labels = self.csv['Retinopathy grade'].values
+        
+        
+        self.mask_folders = {
+            'MA': os.path.join(mask_root, '1. Microaneurysms'),
+            'HE': os.path.join(mask_root, '2. Haemorrhages'),
+            'EX': os.path.join(mask_root, '3. Hard Exudates'),
+            'SE': os.path.join(mask_root, '4. Soft Exudates'),
+            'OD': os.path.join(mask_root, '5. Optic Disc')
+        }
+        
+       
+        self.base_transform = transforms.Compose([
+            transforms.Resize((trainsize, trainsize)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.4337, 0.2123, 0.0755], std=[0.3116, 0.1665, 0.0866])
+        ])
+        
+       
+        self.mask_transform = transforms.Compose([
+            transforms.Resize((trainsize, trainsize)),
+            transforms.ToTensor()
+        ])
+        
+        # Augmentations
+        if self.augmentations:
+            print('Using augmentations')
+            self.aug_transform = transforms.RandomApply([
+                transforms.RandomRotation(90),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomVerticalFlip()
+            ], p=0.5)
+        else:
+            print('No augmentations')
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, index):
+       
+        image = Image.open(self.image_paths[index]).convert('RGB')
+        label = self.labels[index]
+        
+        #loading mask
+        img_id = os.path.splitext(os.path.basename(self.image_paths[index]))[0]
+        masks = []
+        
+        for lesion, folder in self.mask_folders.items():
+            mask_path = os.path.join(folder, f"{img_id}_{lesion}.tif")
+            if os.path.exists(mask_path):
+                mask = np.array(Image.open(mask_path).convert('L'))
+                mask = (mask > 0).astype(np.uint8)
+            else:
+                mask = np.zeros((image.size[1], image.size[0]), dtype=np.uint8)  
+            masks.append(mask)
+        
+        masks = np.stack(masks, axis=0)  # (5, H, W)
+        
+        # Process masks
+        if self.mask_type == 'binary':
+            final_mask = (np.sum(masks, axis=0) > 0).astype(np.uint8)
+            mask_pil = Image.fromarray(final_mask)
+        else:
+            final_mask = masks  # (5, H, W)
+            mask_pil = [Image.fromarray(m) for m in final_mask]
+
+        # Apply base transforms
+        image = self.base_transform(image)
+        
+        if self.mask_type == 'binary':
+            mask = self.mask_transform(mask_pil).float()  # (1, H, W)
+        else:
+            mask = torch.stack([self.mask_transform(m).squeeze() for m in mask_pil]).float()  # (5, H, W)
+
+        return image, torch.tensor(label).long(), mask
+
+# In[ ]:
+
+
+
+
